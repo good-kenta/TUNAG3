4bc76b608ed866b44e48cceb2c1a70a20fd07bf2
GW-239\Education <k.miyamoto@good-works.co.jp>
2023-07-07 16:38:08 +0900

    ホーム（新規会員登録）の画面遷移OK

--- tunag3/app/src/main/java/com/example/tunag3/MainActivity.kt
+++ tunag3/app/src/main/java/com/example/tunag3/MainActivity.kt
@@ -1,19 +1,29 @@
 package com.example.tunag3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.tunag3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+//    private lateinit var binding: ActivityMainBinding
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
 
-        binding.addButton.setOnClickListener{
-            binding.addUserText.text = "ボタンがタップされました。"
+        binding.addButton.setOnClickListener{ addButtonTapped(it) }
+    }
+    fun addButtonTapped(view: View?){
+        val intent = Intent(this, addUser::class.java)
+        startActivity(intent)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        val view = binding.root
+//        setContentView(view)
+
+//        binding.addButton.setOnClickListener{
+//            binding.addUserText.text = "ボタンがタップされました。"
         }
     }
-}
