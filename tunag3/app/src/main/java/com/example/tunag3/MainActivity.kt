de3b32d06b84274cdfbce552063b479f7473a7b5
GW-239\Education <k.miyamoto@good-works.co.jp>
2023-07-07 15:18:38 +0900

    ボタン押下時のテキスト文変化

--- tunag3/app/src/main/java/com/example/tunag3/MainActivity.kt
+++ tunag3/app/src/main/java/com/example/tunag3/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.tunag3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tunag3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.addButton.setOnClickListener{
+            binding.addUserText.text = "ボタンがタップされました。"
+        }
     }
 }
