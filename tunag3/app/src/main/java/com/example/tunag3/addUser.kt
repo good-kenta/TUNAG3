879d41dd45c4b50c9cb9c50efeb66fb001fb9c0d
GW-239\Education <k.miyamoto@good-works.co.jp>
2023-07-07 15:52:37 +0900

    新規ユーザー登録画面作成

---
+++ tunag3/app/src/main/java/com/example/tunag3/addUser.kt
@@ -0,0 +1,11 @@
+package com.example.tunag3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class addUser : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_user)
+    }
+}
